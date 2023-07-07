c8c4f920d96ec27ded050aba9ab234b08c36627c
kmr-sahil <91898837+kmr-sahil@users.noreply.github.com>
2023-07-07 13:13:19 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
       fontFamily: {
         'heebo': ['Heebo', 'sans-serif']
       },
+      screens: {
+        'tablet': '474px',
+        // => @media (min-width: 474px) { ... }
+
+        'tabletlg': '767px',
+        // => @media (min-width: 767px) { ... }
+  
+        'laptop': '991px',
+        // => @media (min-width: 991px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [],
